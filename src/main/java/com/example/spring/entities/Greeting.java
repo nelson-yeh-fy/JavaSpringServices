849a3b866e7934e4081d457e584778a652c3a2fb
nelson-yeh-fy <nelson.yeh.fy@gmail.com>
2022-04-07 20:39:51 -0400

    Initial commit

---
+++ src/main/java/com/example/spring/entities/Greeting.java
@@ -0,0 +1,29 @@
+package com.example.spring.entities;
+
+/**
+ * @author nelson-yeh-fy (https://https://github.com/nelson-yeh-fy)
+ * @version 1.0
+ * @since 1.0
+ */
+public class Greeting {
+    private long id;
+    private String name;
+
+    /**
+     * Implements generic greeting object
+     *
+     * @param id the id
+     * @param name the name string
+     */
+    public Greeting(long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+    public long getId(){
+        return this.id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+}
---
+++ src/test/java/com/example/spring/services/JavaSpringServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
